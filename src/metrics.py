a313589eb545619e76d1d3a5b1c4e724229c855a
lancelotperonnet <lancelot.peronnet@hec.edu>
2022-02-01 18:47:35 +0100

    finii

--- src/metrics.py
+++ src/metrics.py
@@ -1,18 +1,33 @@
 import pandas as pd
+import loading
 
 def valm2_communal(df):
     return df.groupby(['l_codinsee', 'anneemut']).mean()[['valm2']].unstack().reset_index().T.reset_index().T.iloc[2:,:]
 
 
 def fill_na(df, df_2):
-    df = df.merge(df_2[['l_codinsee', 'coddep']], how='left', on='l_codinsee')
-    df = df.merge( pd.DataFrame(df_2.groupby(['coddep']).mean()['valm2'].reset_index() ), how='left', on='coddep')
+    df_commune = loading.load_communal_data()
+    df_commune.columns = ['l_codinsee', 'code_zone', 'no_need']  
+    df_commune.drop(['no_need', 'code_zone'], axis=1, inplace=True)
+    df_commune['l_codinsee'] = df_commune['l_codinsee'].astype(int)
+    df_commune['coddep'] = df_commune['l_codinsee']//1000
+
+    df.l_codinsee = df.l_codinsee.astype(int)  
+     
+    df = df.merge(df_commune,how='right',on='l_codinsee')
+    
+    df_3 = pd.DataFrame(df_2.groupby(['coddep']).mean()['valm2'].reset_index())
+    df_3.coddep = df_3.coddep.astype(int)
+    df.coddep = df.coddep.astype(int)
+
+    df = df.merge( df_3, how='left', on='coddep')
     df.drop(['coddep'], axis=1, inplace=True)
 
     df_to_fill = df.iloc[:, 1:]
-    df_to_fill.fillna(method='ffill', axis=1, inplace=True)
-    df_to_fill.fillna(method='bfill', axis=1, inplace=True)
-    return pd.concat([ df.iloc[:,:1] , df_to_fill], axis=1)
+    df_to_fill = df_to_fill.ffill(axis=1).bfill(axis=1)
+    df_to_fill.drop(['valm2'], axis=1, inplace=True)
+
+    return pd.concat([ df.iloc[:,:1] , df_to_fill], axis=1).drop_duplicates()
 
 
 
@@ -27,7 +42,9 @@ def calculated_data(df_buy, df_sell):
     df_sell_calcul = valm2_communal(df_sell)
     df_sell_calcul.columns = ['l_codinsee','s_14', 's_15', 's_16', 's_17', 's_18', 's_19', 's_20']
     df_sell_calcul = fill_na(df_sell_calcul, df_sell)
+
     df = df.merge(df_sell_calcul, how='left', on='l_codinsee')
+    df.drop_duplicates(inplace=True)
 
     array = df.iloc[:, 8:15].values / df.iloc[:, 1:8].values
     cols = ['ratio_2014', 'ratio_2015', 'ratio_2016', 'ratio_2017', 'ratio_2018', 'ratio_2019', 'ratio_2020']
