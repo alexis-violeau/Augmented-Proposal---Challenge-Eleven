9405e1bdebf93bc56d4ac44c52bf4a7d120615b8
alexis-violeau <alexis.violeau.98@gmail.com>
2022-02-01 12:41:40 +0100

    map app

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from folium import plugins
 from folium.plugins import HeatMap
 from streamlit_folium import folium_static
 
-    
-    
-
 sys.path.append('src/')
 
 import preprocessing
@@ -16,14 +13,27 @@ import loading
 df = loading.load_data()
 df_sell, df_buy = preprocessing.filter_dataset(df)
 
-st.write('Hello World !')
+st.title('Opportunities in the real estate landscape')
 
+type = st.radio(
+     "Type of mutation",
+     ('Appartment', 'Raw land'))
 
-map_sell = folium.Map(location=[48.868229, 2.347402],
+if type == 'Appartment':
+    map_sell = folium.Map(location=[48.868229, 2.347402],
                     zoom_start = 12)
+    coords_sell = df_sell[['latitude', 'longitude']]
+    coords_sell = coords_sell.dropna(axis=0, subset=['latitude','longitude'])
+    coords_sell = [[row['latitude'],row['longitude']] for index, row in coords_sell.iterrows()]
+    HeatMap(coords_sell).add_to(map_sell)
 
-folium_static(map_sell)
-
-
-
-#st.map(df_sell[['latitude','longitude','valm2']])
+    folium_static(map_sell)
+else:
+    map_buy = folium.Map(location=[48.868229, 2.347402],
+                    zoom_start = 12)
+    coords_buy = df_buy[['latitude', 'longitude']]
+    coords_buy = coords_buy.dropna(axis=0, subset=['latitude','longitude'])
+    coords_buy = [[row['latitude'],row['longitude']] for index, row in coords_buy.iterrows()]
+    HeatMap(coords_buy).add_to(map_buy)
+    
+    folium_static(map_buy)
