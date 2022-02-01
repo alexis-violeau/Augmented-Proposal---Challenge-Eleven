6f41acb83bebe87562d8f72f2ae16f859498ee34
lancelotperonnet <lancelot.peronnet@hec.edu>
2022-02-01 20:08:09 +0100

    girtignore

--- app.py
+++ app.py
@@ -1,30 +1,76 @@
 import sys
+import numpy as np
+from model import X_COLS_BUY
 import streamlit as st 
 import folium
 from folium import plugins
 from folium.plugins import HeatMap
 from streamlit_folium import folium_static
+import geopandas as gpd
 
 sys.path.append('src/')
+
+st.set_page_config(layout="wide")
 
 import preprocessing
 import loading
 import visualization
+import model
 
 df = loading.load_data()
 df_sell, df_buy = preprocessing.filter_dataset(df)
 
-st.title('Opportunities in the real estate landscape')
+st.sidebar.title('Opportunities in the real estate landscape')
 
-type = st.radio(
+st.sidebar.header('Map')
+
+type = st.sidebar.radio(
      "Type of mutation",
      ('Appartment', 'Raw land'))
 
 if type == 'Appartment':
-    visualization.density_maps(df_sell)
+    df_selected = df_sell
 else:
-    visualization.density_maps(df_buy)
+    df_selected = df_buy
+
+col = st.sidebar.selectbox(
+     'KPI',
+     ('trade density','valm2', 'return', 'change in return'))
+
+year_min,year_max = st.sidebar.slider('Select year range :',2014, 2020, (2016, 2018))
+
+if col == 'trade density':
+    df_selected = df_selected[['anneemut','insee']]
+    df_agg = df_selected[(df_selected['anneemut'] >= year_min) & (df_selected['anneemut'] <= year_max)].groupby('insee').count().reset_index()
+    df_geo = gpd.GeoDataFrame(preprocessing.add_geodata(df_agg).round())
+    map = visualization.communal_maps(communal_df=df_geo,col='anneemut',legend='Number of trade')
     
-visualization.communal_maps(df_sell.groupby('insee').mean(),'valm2')
+if col == 'valm2':
+    df_selected = df_selected[['anneemut','insee',col]]
+    df_agg = df_selected[(df_selected['anneemut'] >= year_min) & (df_selected['anneemut'] <= year_max)].groupby('insee').mean().reset_index()
+    df_geo = gpd.GeoDataFrame(preprocessing.add_geodata(df_agg).round())
+    map = visualization.communal_maps(communal_df=df_geo,col=col,legend='Average square meter price')
     
-    
+st.sidebar.header('The Right Price')
+
+map.add_child(folium.ClickForMarker(popup='Sell price (/m2) = '))
+
+
+
+adresse = st.sidebar.text_input('Adress : ', '11 Rue Vieille du Temple, Paris')
+
+lat,long = visualization.extract_logitude_latitude(adresse)
+
+model_buy = model.load_buy_model()
+model_sell = model.load_sell_model()
+
+price_sell = model_sell.predict(np.array([[lat,long,1.0,0.0]]))
+price_buy = model_buy.predict(np.array([[lat,long,0.0]]))
+
+
+folium.Marker(
+      location=[lat, long],
+      popup=adresse +  ' \n sell price : ' + str(price_sell[0]) + ' \n buy price : ' + str(price_buy[0]),
+   ).add_to(map)
+
+folium_static(map)
--- src/visualization.py
+++ src/visualization.py
@@ -4,38 +4,37 @@ import folium
 from folium import plugins
 from folium.plugins import HeatMap
 from streamlit_folium import folium_static
+from geopy.geocoders import Nominatim
 
 
-def communal_maps(communal_df, col, legend=''):
+def communal_maps(communal_df, col, legend = ''):
     communal_map = folium.Map(location=[48.868229, 2.347402],
                           zoom_start = 10,
                           tiles = 'cartodbpositron')
-
     myscale = (communal_df[col].quantile((0,0.2,0.75,0.9,0.98,1))).tolist()
-    folium.Choropleth(
-        geo_data = communal_df,                  #json
-        name ='choropleth',                  
-        data = communal_df,                     
-        columns = ['insee',col], #columns to work on
-        key_on ='feature.properties.insee',
-        fill_color ='YlGnBu',     #I passed colors Yellow,Green,Blue
-        threshold_scale=myscale,
-        fill_opacity = 0.7,
-        line_opacity = 0.2,
-       legend_name = legend
-    ).add_to(communal_map)
-
+    choropleth = folium.Choropleth(
+                    geo_data = communal_df,                  #json
+                    name ='choropleth',
+                    data = communal_df,
+                    columns = ['insee', col], #columns to work on
+                    key_on ='feature.properties.insee',
+                    fill_color = 'YlGnBu',     #I passed colors Yellow,Green,Blue
+                    threshold_scale = myscale,
+                    fill_opacity = 0.7,
+                    line_opacity = 0.4,
+                   legend_name = legend,
+                    highlight = True)
+    choropleth.add_to(communal_map)
     folium.LayerControl().add_to(communal_map)
+    choropleth.geojson.add_child(
+        folium.features.GeoJsonTooltip(['insee', col], aliases=['Postal Code : ', legend + ' : '])
+    )
+    return communal_map
 
-    return folium_static(communal_map)
-
-
-def density_maps(df):
-    map = folium.Map(location=[48.868229, 2.347402],
-                    zoom_start = 12)
-    coords = df[['latitude', 'longitude']]
-    coords = coords.dropna(axis=0, subset=['latitude','longitude'])
-    coords = [[row['latitude'],row['longitude']] for index, row in coords.iterrows()]
-    HeatMap(coords).add_to(map)
-
-    return folium_static(map)
+def extract_logitude_latitude(adresse):
+    geolocator = Nominatim(user_agent = 'thomas_b')
+    location = geolocator.geocode(adresse)
+    print(location)
+    if location is None :
+        raise NameError("Erreur d'adresse")
+    return location.latitude, location.longitude
